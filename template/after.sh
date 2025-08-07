323e0ba89d7a8b14b52323fdb9d8186a02f73254
Jeremy Guillette <3651377+jaguillette@users.noreply.github.com>
2025-08-07 16:05:35 -0400

    set up consistent and useful logging

--- template/after.sh
+++ template/after.sh
@@ -1,12 +1,20 @@
+#!/usr/bin/env bash
+
+this_script=$0
+
+log() {
+    echo -e "[$(date -Iseconds)][${this_script}] $1"
+}
+
 # Wait for the Jupyter Notebook server to start
-echo "Waiting for Jupyter Notebook server to open port ${port}..."
-echo "TIMING - Starting wait at: $(date)"
+log "Waiting for Jupyter Notebook server to open port ${port}..."
+log "Starting wait..."
 if wait_until_port_used "${host}:${port}" 600; then
-  echo "Discovered Jupyter Notebook server listening on port ${port}!"
-  echo "TIMING - Wait ended at: $(date)"
+  log "Discovered Jupyter Notebook server listening on port ${port}!"
+  log "Wait ended"
 else
-  echo "Timed out waiting for Jupyter Notebook server to open port ${port}!"
-  echo "TIMING - Wait ended at: $(date)"
+  log "Timed out waiting for Jupyter Notebook server to open port ${port}!"
+  log "Wait ended at"
   pkill -P ${SCRIPT_PID}
   clean_up 1
 fi
